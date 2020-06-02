d38daa0d5bdeae36921e7e86062e910cf9b3c99c
igroman787 <igroman787@users.noreply.github.com>
2020-06-02 13:37:01 +0300

    debugging installation script

--- mytoncore.py
+++ mytoncore.py
@@ -9,6 +9,10 @@ from mypylib.mypylib import *
 local = MyPyClass(__file__)
 print(bcolors.Red(json.dumps(sys.argv)))
 
+import getpass
+username = getpass.getuser()
+print(bcolors.Red(username))
+
 
 class LiteClient:
 	def __init__(self):
