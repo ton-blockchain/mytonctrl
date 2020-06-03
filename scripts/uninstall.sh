928cced390c99d448b43b31a778226068fe7f457
igroman787 <igroman787@users.noreply.github.com>
2020-06-03 11:32:31 +0300

    debugging installation script

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -7,6 +7,11 @@ if [ "$(id -u)" != "0" ]; then
 	exit 1
 fi
 
+# Цвета
+COLOR='\033[92m'
+ENDC='\033[0m'
+
+# Удаление файлов
 rm -rf /usr/src/ton
 rm -rf /usr/src/mytonctrl
 rm -rf /usr/bin/ton
@@ -15,3 +20,12 @@ rm -rf /tmp/myton*
 rm -rf /usr/loca/bin/myton*
 rm -rf ~/.local/share/mytonctrl
 rm -rf ~/.local/share/mytoncore/mytoncore.db
+
+# Удаление ссылок
+rm -rf /usr/bin/fift
+rm -rf /usr/bin/liteclient
+rm -rf /usr/bin/validator-console
+rm -rf /usr/bin/mytonctrl
+
+# Конец
+echo -e "${COLOR}Uninstall Complete${ENDC}"
