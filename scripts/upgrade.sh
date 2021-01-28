0fa23356588572db8108377a5e31c8b5bad2c501
igroman787 <igroman787@users.noreply.github.com>
2021-01-28 23:12:17 +0300

    upgrade.sh bugfix

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -17,6 +17,7 @@ export CC=/usr/bin/clang
 export CXX=/usr/bin/clang++
 export CCACHE_DISABLE=1
 
+mkdir -p /tmp/mytoninstaller/
 mv /usr/bin/ton/validator-engine-console/client /tmp/mytoninstaller/client
 mv /usr/bin/ton/validator-engine-console/client.pub /tmp/mytoninstaller/client.pub
 mv /usr/bin/ton/validator-engine-console/server.pub /tmp/mytoninstaller/server.pub
