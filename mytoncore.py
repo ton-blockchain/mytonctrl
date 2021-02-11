ea84a492b5f90f067f0b90d329cc9ed7dfb62ee6
igroman787 <igroman787@users.noreply.github.com>
2021-02-11 09:24:43 +0300

    bugfix

--- mytoncore.py
+++ mytoncore.py
@@ -1954,7 +1954,8 @@ class MyTonCore():
 			if adnlAddr == searchAdnlAddr:
 				mr = validator.get("mr")
 				wr = validator.get("wr")
-				r = (mr + wr) / 2 * 100
+				r = (mr + wr) / 2
+				efficiency = round(r * 100, 2)
 				return r
 		local.AddLog("GetValidatorEfficiency warning: efficiency not found.", "warning")
 	#end define
