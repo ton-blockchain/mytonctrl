18f7b0fd928b998615ac12543cd46499695f97f4
igroman787 <igroman787@users.noreply.github.com>
2021-02-20 22:28:46 +0300

    bugfix

--- scripts/upgrade.py
+++ scripts/upgrade.py
@@ -1,5 +1,6 @@
 # Говнокод ON
 import os
+import subprocess
 from sys import path
 from os.path import dirname as dir
 path.append(dir(path[0]))
@@ -18,10 +19,15 @@ args = ["systemctl", "disable", "validator2"]
 subprocess.run(args)
 
 #fix me
-vuser = "validator"
 try:
+	args = ["systemctl", "stop", "validator"]
+	subprocess.run(args)
 	os.remove("/etc/systemd/system/validator.service")
+	args = ["systemctl", "daemon-reload"]
+	subprocess.run(args)
 except: pass
 cmd = "{validatorAppPath} --daemonize --global-config {globalConfigPath} --db {tonDbDir} --logname {tonLogPath} --state-ttl 172800 --block-ttl 1814400 --archive-ttl 3153600000 --key-proof-ttl 3153600000 --sync-before 3600000 --verbosity 1"
 cmd = cmd.format(validatorAppPath=validatorAppPath, globalConfigPath=globalConfigPath, tonDbDir=tonDbDir, tonLogPath=tonLogPath)
-Add2Systemd(name="validator", user=vuser, start=cmd) # post="/usr/bin/python3 /usr/src/mytonctrl/mytoncore.py -e \"validator down\""
+Add2Systemd(name="validator", user="validator", start=cmd) # post="/usr/bin/python3 /usr/src/mytonctrl/mytoncore.py -e \"validator down\""
+args = ["systemctl", "start", "validator"]
+subprocess.run(args)
