be4c4b427ee7ddea6e06253e7624f85b7f58f48d
igroman787 <igroman787@users.noreply.github.com>
2021-02-20 21:53:40 +0300

    bugfix

--- scripts/upgrade.py
+++ scripts/upgrade.py
@@ -1,4 +1,5 @@
 # Говнокод ON
+import os
 from sys import path
 from os.path import dirname as dir
 path.append(dir(path[0]))
@@ -15,3 +16,9 @@ cmd = "{validatorAppPath} -d -C {globalConfigPath} --db {tonDbDir} -l {tonLogPat
 Add2Systemd(name="validator2", user="validator", start=cmd)
 args = ["systemctl", "disable", "validator2"]
 subprocess.run(args)
+
+#fix me
+os.remove("/etc/systemd/system/validator.service")
+cmd = "{validatorAppPath} --daemonize --global-config {globalConfigPath} --db {tonDbDir} --logname {tonLogPath} --state-ttl 172800 --block-ttl 1814400 --archive-ttl 3153600000 --key-proof-ttl 3153600000 --sync-before 3600000 --verbosity 1"
+cmd = cmd.format(validatorAppPath=validatorAppPath, globalConfigPath=globalConfigPath, tonDbDir=tonDbDir, tonLogPath=tonLogPath)
+Add2Systemd(name="validator", user=vuser, start=cmd) # post="/usr/bin/python3 /usr/src/mytonctrl/mytoncore.py -e \"validator down\""
