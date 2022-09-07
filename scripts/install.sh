3d3cf3aef0f395f0b84d069bd05b41dec87fff8c
K-Dimentional Tree <kdimentionaltree@gmail.com>
2022-09-07 23:15:52 +0000

    WIP2

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-# Проверить sudo
+# colors
+COLOR='\033[92m'
+ENDC='\033[0m'
+
+# check sudo permissions
 if [ "$(id -u)" != "0" ]; then
     echo "Please run script as root"
     exit 1
@@ -12,12 +16,12 @@ repo=${TON_REPO:-mytonctrl}
 branch=${TON_BRANCH:-master}
 
 # node install parameters
-config=${TON_CONFIG:-https://ton-blockchain.github.io/global.config.json}
+config="https://ton-blockchain.github.io/global.config.json"
 telemetry=true
 ignore=false
 dump=false
 
-while getopts m:c:tidT flag
+while getopts m:c:tid flag
 do
 	case "${flag}" in
 		m) mode=${OPTARG};;
@@ -25,17 +29,12 @@ do
 		t) telemetry=false;;
 		i) ignore=true;;
 		d) dump=true;;
-		T) testnet=true;;
 	esac
 done
 
-# Проверка режима установки
-if [ "${mode}" != "lite" ] && [ "${mode}" != "full" ]; then
-	echo "Run script with flag '-m lite' or '-m full'"
-	exit 1
-fi
+# check machine configuration
+echo -e "${COLOR}[1/5]${ENDC} Checking system requirements"
 
-# Проверка мощностей
 cpus=$(lscpu | grep "CPU(s)" | head -n 1 | awk '{print $2}')
 memory=$(cat /proc/meminfo | grep MemTotal | awk '{print $2}')
 
@@ -49,39 +48,46 @@ if [ "${mode}" = "full" ] && [ "$ignore" = false ] && ([ "${cpus}" -lt 8 ] || [ 
 	exit 1
 fi
 
-# Цвета
-COLOR='\033[92m'
-ENDC='\033[0m'
-
-# Начинаю установку mytonctrl
-echo -e "${COLOR}[1/4]${ENDC} Starting installation MyTonCtrl"
-mydir=$(pwd)
-
-
-# На OSX нет такой директории по-умолчанию, поэтому создаем...
+echo -e "${COLOR}[2/5]${ENDC} Checking for required TON components"
 SOURCES_DIR=/usr/src
 BIN_DIR=/usr/bin
+
+# create dirs for OSX
 if [[ "$OSTYPE" =~ darwin.* ]]; then
 	SOURCES_DIR=/usr/local/src
 	BIN_DIR=/usr/local/bin
 	mkdir -p ${SOURCES_DIR}
 fi
 
-# Проверяю наличие компонентов TON
-echo -e "${COLOR}[2/4]${ENDC} Checking for required TON components"
+# check TON components
 file1=${BIN_DIR}/ton/crypto/fift
 file2=${BIN_DIR}/ton/lite-client/lite-client
 file3=${BIN_DIR}/ton/validator-engine-console/validator-engine-console
 
 if  [ ! -f "${file1}" ] || [ ! -f "${file2}" ] || [ ! -f "${file3}" ]; then
-	echo "Ton does not exists, building"
-else
+	echo "TON does not exists, building"
 	wget https://raw.githubusercontent.com/${author}/${repo}/${branch}/scripts/ton_installer.sh -O /tmp/ton_installer.sh
 	bash /tmp/ton_installer.sh -c ${config}
-	rm -f toninstaller.sh
 fi
 
 # Cloning mytonctrl
+echo -e "${COLOR}[3/5]${ENDC} Installing MyTonCtrl"
 cd $SOURCES_DIR
 rm -rf $SOURCES_DIR/mytonctrl
-git clone --recursive https://github.com/ton-blockchain/mytonctrl.git
+
+git clone --recursive https://github.com/${author}/${repo}.git mytonctrl
+cd $SOURCES_DIR/mytonctrl
+git checkout ${branch}
+
+echo -e "${COLOR}[3/5]${ENDC} Running myton.installer"
+# check installation mode
+if [ "${mode}" != "lite" ] && [ "${mode}" != "full" ]; then
+	echo "Run script with flag '-m lite' or '-m full'"
+	exit 1
+fi
+
+user=$(ls -lh ${mydir}/${0} | cut -d ' ' -f 3)
+python3 -m myton.installer -m ${mode} -u ${user} -t ${telemetry} --dump ${dump}
+
+echo -e "${COLOR}[4/4]${ENDC} Mytonctrl installation completed"
+exit 0
