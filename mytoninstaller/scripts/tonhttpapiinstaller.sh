d8f617dc6d08fdf92ad8370223e4739a3939d922
K-Dimentional Tree <kdimenionaltree@gmail.com>
2023-02-17 17:40:48 +0000

    Updated mtc-jsonrpc installer

--- mytoninstaller/scripts/tonhttpapiinstaller.sh
+++ mytoninstaller/scripts/tonhttpapiinstaller.sh
@@ -20,30 +20,19 @@ COLOR='\033[92m'
 ENDC='\033[0m'
 
 # Установка компонентов python3
-echo -e "${COLOR}[1/4]${ENDC} Installing required packages"
-pip3 install pipenv==2022.3.28
-
-# Клонирование репозиториев с github.com
-echo -e "${COLOR}[2/4]${ENDC} Cloning github repository"
-cd /usr/src
-rm -rf pytonv3
-#git clone https://github.com/EmelyanenkoK/pytonv3
-git clone https://github.com/igroman787/pytonv3
+echo -e "${COLOR}[1/3]${ENDC} Installing required packages"
+pip3 install -U ton-http-api
 
 # Установка модуля
-cd /usr/src/pytonv3
-python3 setup.py install
+echo -e "${COLOR}[2/3]${ENDC} Add to startup"
+mkdir -p /var/ton-http-api/ton_keystore/
+chown -R $user /var/ton-http-api/
 
-# Скомпилировать недостающий бинарник
-cd /usr/bin/ton && make tonlibjson
-
-# Прописать автозагрузку
-echo -e "${COLOR}[3/4]${ENDC} Add to startup"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo "Script dir: ${SCRIPT_DIR}"
-${SCRIPT_DIR}/add2systemd -n pytonv3 -s "/usr/bin/python3 -m pyTON --liteserverconfig /usr/bin/ton/local.config.json --libtonlibjson /usr/bin/ton/tonlib/libtonlibjson.so" -u ${user} -g ${user}
-systemctl restart pytonv3
+cmd="ton-http-api --port=8000 --logs-level=INFO --cdll-path=/usr/bin/ton/tonlib/libtonlibjson.so --liteserver-config /usr/bin/ton/local.config.json --tonlib-keystore=/var/ton-http-api/ton_keystore/ --parallel-requests-per-liteserver=1024"
+${SCRIPT_DIR}/add2systemd.sh -n ton-http-api -s "${cmd}" -u ${user} -g ${user}
+systemctl restart ton-http-api
 
 # Конец
-echo -e "${COLOR}[4/4]${ENDC} pyTONv3 installation complete"
+echo -e "${COLOR}[3/3]${ENDC} TonHttpApi installation complete"
 exit 0
