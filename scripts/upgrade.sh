2556501525176f0f9faf1cb61b630ec3bda8f4e0
neodiX <neodix@ton.org>
2023-06-19 17:44:21 +0200

    upgrade with ninja

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -28,7 +28,7 @@ done
 COLOR='\033[92m'
 ENDC='\033[0m'
 
-# Установить дополниьтельные зависимости
+# Установить дополнительные зависимости
 apt-get install -y libsecp256k1-dev libsodium-dev
 
 # Go to work dir
@@ -44,12 +44,14 @@ export CXX=/usr/bin/clang++
 export CCACHE_DISABLE=1
 
 # Update binary
-cd ${bindir}/${repo}
-rm -f CMakeCache.txt
+cd ${bindir}
+rm -rf ${repo}
+mkdir ${repo}
+cd ${repo}
 memory=$(cat /proc/meminfo | grep MemAvailable | awk '{print $2}')
 let "cpuNumber = memory / 2100000" || cpuNumber=1
-cmake -DCMAKE_BUILD_TYPE=Release ${srcdir}/${repo}
-make -j ${cpuNumber} fift validator-engine lite-client pow-miner validator-engine-console generate-random-id dht-server func tonlibjson rldp-http-proxy
+cmake -DCMAKE_BUILD_TYPE=Release ${srcdir}/${repo} -GNinja
+ninja -j ${cpuNumber} fift validator-engine lite-client pow-miner validator-engine-console generate-random-id dht-server func tonlibjson rldp-http-proxy
 systemctl restart validator
 
 # Конец
