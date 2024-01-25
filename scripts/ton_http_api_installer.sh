9c574c364226064a8a5244389a374b3448e3b274
Igroman787 <27614297+igroman787@users.noreply.github.com>
2024-01-25 17:33:12 +0300

    bugfix

--- scripts/ton_http_api_installer.sh
+++ scripts/ton_http_api_installer.sh
@@ -31,7 +31,7 @@ echo -e "${COLOR}[3/4]${ENDC} Add to startup"
 venv_ton_http_api="${venv_path}/bin/ton-http-api"
 tonlib_path="/usr/bin/ton/tonlib/libtonlibjson.so"
 ls_config="/usr/bin/ton/localhost.config.json"
-cmd="from sys import path; path.append('/usr/src/mytonctrl/'); from mypylib.mypylib import Add2Systemd; Add2Systemd(name='ton_http_api', user='${user}', start='${venv_ton_http_api} --host 127.0.0.1 --port 8801 --liteserver-config ${ls_config} --cdll-path ${tonlib_path} --tonlib-keystore /tmp/tonlib_keystore/')"
+cmd="from sys import path; path.append('/usr/src/mytonctrl/'); from mypylib.mypylib import add2systemd; add2systemd(name='ton_http_api', user='${user}', start='${venv_ton_http_api} --host 127.0.0.1 --port 8801 --liteserver-config ${ls_config} --cdll-path ${tonlib_path} --tonlib-keystore /tmp/tonlib_keystore/')"
 python3 -c "${cmd}"
 systemctl restart ton_http_api
 
