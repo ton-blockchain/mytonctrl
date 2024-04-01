13ffb279d9d89f1f85f3e42e41c3876633f1832a
yungwine <cyrbatoff@gmail.com>
2024-04-01 19:46:37 +0800

    fix set node args

--- mytoninstaller/node_args.py
+++ mytoninstaller/node_args.py
@@ -1,4 +1,3 @@
-from mypylib.mypylib import run_as_root
 
 
 def get_validator_service():
@@ -34,36 +33,3 @@ def get_node_args(command: str = None):
     return result
 #end define
 
-
-def restart_node():
-    exit_code = run_as_root(["systemctl", "daemon-reload"])
-    if exit_code:
-        raise Exception(f"`systemctl daemon-reload` failed with exit code {exit_code}")
-    exit_code = run_as_root(["systemctl", "restart", "validator"])
-    if exit_code:
-        raise Exception(f"`systemctl restart validator` failed with exit code {exit_code}")
-#end define
-
-
-def set_node_arg(arg_name: str, arg_value: str = ''):
-    """
-    :param arg_name:
-    :param arg_value: arg value. if None, remove the arg; if empty string, argument is set without value
-    :return:
-    """
-    assert arg_name.startswith('-'), 'arg_name must start with "-" or "--"'
-    service = get_validator_service()
-    command = get_node_start_command()
-    if command is None:
-        raise Exception('Cannot find node start command in service file')
-    args = get_node_args(command)
-    if arg_value is None:
-        args.pop(arg_name, None)
-    else:
-        args[arg_name] = arg_value
-    new_command = command.split(' ')[0] + ' ' + ' '.join([f'{k} {v}' for k, v in args.items()])
-    new_service = service.replace(command, new_command)
-    c = f"with open('/etc/systemd/system/validator.service', 'w') as f: f.write('''{new_service}''')"
-    run_as_root(['python3', '-c', f'"{c}"'])
-    restart_node()
-#end define
