86dcdbb954b75f8cd869de8790f87108ca60e650
yungwine <cyrbatoff@gmail.com>
2024-06-19 13:58:03 +0700

    add try

--- mytonctrl.py
+++ mytonctrl.py
@@ -231,10 +231,13 @@ def CheckMytonctrlUpdate():
 
 
 def CheckMytonctrl2Update():
-	if not ton.find_myself_in_vl():  # we are not validator in current and prev rounds
-		print('============================================================================================')
-		color_print(local.translate("update_mtc2_warning"))
-		print('============================================================================================')
+	try:
+		if not ton.find_myself_in_vl():  # we are not validator in current and prev rounds
+			print('============================================================================================')
+			color_print(local.translate("update_mtc2_warning"))
+			print('============================================================================================')
+	except Exception as err:
+		print(f'Failed to check node as validator: {err}')
 # end define
 
 
