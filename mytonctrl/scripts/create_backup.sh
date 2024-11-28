c00f8c37226a6738cd5cd44357bcb300ad27e5ee
yungwine <cyrbatoff@gmail.com>
2024-11-28 19:35:21 +0900

    add ton_dir option to create_backup.sh

--- mytonctrl/scripts/create_backup.sh
+++ mytonctrl/scripts/create_backup.sh
@@ -1,12 +1,14 @@
 dest="mytonctrl_backup_$(hostname)_$(date +%s).tar.gz"
 mtc_dir="$HOME/.local/share/mytoncore"
 user=$(logname)
+ton_dir="/var/ton-work"
 # Get arguments
-while getopts d:m: flag
+while getopts d:m:t: flag
 do
 	case "${flag}" in
 		d) dest=${OPTARG};;
     m) mtc_dir=${OPTARG};;
+    t) ton_dir=${OPTARG};;
     *)
         echo "Flag -${flag} is not recognized. Aborting"
         exit 1 ;;
@@ -26,9 +28,9 @@ rm -rf $tmp_dir
 mkdir $tmp_dir
 mkdir $tmp_dir/db
 
-cp /var/ton-work/db/config.json ${tmp_dir}/db
-cp -r /var/ton-work/db/keyring ${tmp_dir}/db
-cp -r /var/ton-work/keys ${tmp_dir}
+cp $ton_dir/db/config.json ${tmp_dir}/db
+cp -r $ton_dir/db/keyring ${tmp_dir}/db
+cp -r $ton_dir/keys ${tmp_dir}
 cp -r $mtc_dir $tmp_dir
 
 python3 -c "import json;f=open('${tmp_dir}/db/config.json');json.load(f);f.close()" || exit 1  # Check if config.json is copied correctly
