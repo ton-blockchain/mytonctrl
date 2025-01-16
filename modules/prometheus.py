8b080e6f4b029f8de8a94279575551678ca4154e
yungwine <cyrbatoff@gmail.com>
2025-01-16 20:27:05 +0400

    add validator_validation_metrics

--- modules/prometheus.py
+++ modules/prometheus.py
@@ -19,9 +19,11 @@ class Metric:
 
 METRICS = {
     'master_out_of_sync': Metric('validator_masterchain_out_of_sync_seconds', 'Time difference between current time and timestamp of the last known block', 'gauge'),
-    'shard_out_of_sync': Metric('validator_shardchain_out_of_sync_blocks', 'Number of blocks validator\'s shardclient is behind the last known block', 'gauge'),
+    'shard_out_of_sync': Metric('validator_shardchain_out_of_sync_blocks', 'Number of blocks node\'s shardclient is behind the last known block', 'gauge'),
     'out_of_ser': Metric('validator_out_of_serialization', 'Number of blocks last state serialization was ago', 'gauge'),
-    'vc_up': Metric('validator_console_up', 'Is validator\'s validator client up', 'gauge'),
+    'vc_up': Metric('validator_console_up', 'Is `validator-console` up', 'gauge'),
+    'validator_id': Metric('validator_index', 'Validator index', 'gauge'),
+    'validator_stake': Metric('validator_stake', 'Validator stake', 'gauge'),
 }
 
 
@@ -33,9 +35,8 @@ class PrometheusModule(MtcModule):
     def __init__(self, ton, local, *args, **kwargs):
         super().__init__(ton, local, *args, **kwargs)
 
-    def get_validator_status_metrics(self):
+    def get_validator_status_metrics(self, result: list):
         status = self.ton.GetValidatorStatus()
-        result = []
         if status.masterchain_out_of_sync is not None:
             result.append(METRICS['master_out_of_sync'].to_format(status.masterchain_out_of_sync))
         if status.shardchain_out_of_sync is not None:
@@ -43,7 +44,18 @@ class PrometheusModule(MtcModule):
         if status.masterchain_out_of_ser is not None:
             result.append(METRICS['out_of_ser'].to_format(status.masterchain_out_of_ser))
         result.append(METRICS['vc_up'].to_format(int(status.is_working)))
-        return result
+
+    def get_validator_validation_metrics(self, result: list):
+        index = self.ton.GetValidatorIndex()
+        result.append(METRICS['validator_id'].to_format(index))
+        config = self.ton.GetConfig34()
+        save_elections = self.ton.GetSaveElections()
+        elections = save_elections.get(str(config["startWorkTime"]))
+        if elections is not None:
+            adnl = self.ton.GetAdnlAddr()
+            stake = elections.get(adnl, {}).get('stake')
+            if stake:
+                result.append(METRICS['validator_stake'].to_format(round(stake, 2)))
 
     def push_metrics(self):
         if not self.ton.using_prometheus():
@@ -52,7 +64,9 @@ class PrometheusModule(MtcModule):
         url = self.ton.local.db.get('prometheus_url')
         if url is None:
             raise Exception('Prometheus url is not set')
-        metrics = self.get_validator_status_metrics()
+        metrics = []
+        self.local.try_function(self.get_validator_status_metrics, args=[metrics])
+        self.local.try_function(self.get_validator_validation_metrics, args=[metrics])
         requests.post(url, data='\n'.join(metrics).encode())
 
     def add_console_commands(self, console):
