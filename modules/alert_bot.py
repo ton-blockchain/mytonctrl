399b6b00d5e68010d678ccd769c2f1a5a6453c39
yungwine <cyrbatoff@gmail.com>
2025-01-31 21:45:33 +0400

    fix voting alert

--- modules/alert_bot.py
+++ modules/alert_bot.py
@@ -338,7 +338,7 @@ Alert text:
         if not self.ton.using_validator():
             return
         config = self.ton.GetConfig32()
-        if not (config['endWorkTime'] + FREEZE_PERIOD + 1800 <= time.time() < config['endWorkTime'] + FREEZE_PERIOD + 1860):  # check between 25th and 26th minutes after stakes have been unfrozen
+        if not (config['endWorkTime'] + FREEZE_PERIOD + 1800 <= time.time() < config['endWorkTime'] + FREEZE_PERIOD + 1860):  # check between 30th and 31st minutes after stakes have been unfrozen
             return
         res = self.get_myself_from_election(config)
         if not res:
@@ -356,6 +356,9 @@ Alert text:
             return
         validator_index = self.ton.GetValidatorIndex()
         if validator_index == -1:
+            return
+        config = self.ton.GetConfig34()
+        if time.time() - config['startWorkTime'] < 600:  # less than 10 minutes passed since round start
             return
         need_to_vote = []
         offers = self.ton.GetOffers()
